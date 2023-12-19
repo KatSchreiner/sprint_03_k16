2b86e02b33be78fa803b7828f507631cd1ac3a58
KatSchreiner <kat.schreiner@yandex.ru>
2023-12-19 21:53:58 +0300

    Adding new files

---
+++ sprint_03_k06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sprint_03_k06
+//
+//  Created by Екатерина Шрайнер on 19.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
