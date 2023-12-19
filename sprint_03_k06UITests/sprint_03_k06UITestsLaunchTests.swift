616ac32fdcc476dc3a597715ceb6f6e0e0a2da92
KatSchreiner <kat.schreiner@yandex.ru>
2023-12-19 22:13:37 +0300

    Deleted files and added new

---
+++ sprint_03_k06UITests/sprint_03_k06UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sprint_03_k06UITestsLaunchTests.swift
+//  sprint_03_k06UITests
+//
+//  Created by Екатерина Шрайнер on 19.12.2023.
+//
+
+import XCTest
+
+final class sprint_03_k06UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
